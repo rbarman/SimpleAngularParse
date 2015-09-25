45a653ffd1506e7caaef5d9434c014e2118904a7
Rohan Barman <Rohan1Barman@gmail.com>
2015-09-25 00:09:08 -0400

    renamed vars in GameService.addGame() for clarity

--- public/services/GameService.js
+++ public/services/GameService.js
@@ -8,15 +8,15 @@ myApp.service('GameService', function(){
     var Game = Parse.Object.extend("Game");
     // Initialize a new Game object and assign all relevant fields
     // fields must match the model fields that controller sends
-    var game = new Game();
-    game.set("name" , game.name);
+    var _game = new Game();
+    _game.set("name" , game.name);
     // Save newly created and set game
-    game.save(null, {
+    _game.save(null, {
       // success and error callbacks
-      success: function(game) {
-        console.log('New object created with objectId: ' + game.id);
+      success: function(_game) {
+        console.log('New object created with objectId: ' + _game.id);
       },
-      error: function(game, error) {
+      error: function(_game, error) {
         console.log('Failed to create new object, with error code: ' + error.message);
       }
     });
