5fbb62dfe761d11ea21e4d3d0d73ed94fc273626
Rohan Barman <Rohan1Barman@gmail.com>
2015-12-06 14:47:45 -0500

    removed routeprovider from app.js * simple example does not need routeprovider

--- public/app.js
+++ public/app.js
@@ -1,16 +1,3 @@
-var myApp = angular.module('myApp',['ngRoute']);
+var myApp = angular.module('myApp',[]);
 // Initialize Parse only once
 Parse.initialize("Wm23Levy1L1nw1Pv05wWH8xoSOnKxiK8Ru1cu7Vo", "EXv7bOsmLn0fm9klZ8seRV4yQM6Eo1Jbjup61BSG");
-
-myApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-    when('/',{
-      templateUrl:'views/home.html',
-      controller:'HomeCtrl',
-      controllerAs:''
-    }).
-    otherwise({
-      redirectTo:'/'
-    });
-}]);
